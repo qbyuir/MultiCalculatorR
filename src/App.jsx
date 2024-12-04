43ea82a081b8ee3e3a509caeada91cb0faaf9a03
yuriq <yuriqbarbosa@gmail.com>
2024-12-04 20:56:37 -0300

    finished final details

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { Multicalculator } from './components/Multicalculator'
+import Matcalc from './components/matCalc'
 
 function App() {
   
   
   return (
-    <Multicalculator />
+    <Matcalc />
   )
 }
 
